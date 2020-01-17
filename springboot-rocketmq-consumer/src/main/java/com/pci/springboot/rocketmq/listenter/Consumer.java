79d955c04f9a1ca71318adf174c41041314cb310
zhouyunting <jason@163.com>
2020-01-17 16:33:36 +0800

    修改rocketmq的ip地址

--- springboot-rocketmq-consumer/src/main/java/com/pci/springboot/rocketmq/listenter/Consumer.java
+++ springboot-rocketmq-consumer/src/main/java/com/pci/springboot/rocketmq/listenter/Consumer.java
@@ -6,14 +6,18 @@ import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
 import org.apache.rocketmq.spring.core.RocketMQListener;
 import org.springframework.stereotype.Component;
 
+
 @Component
 @Slf4j
 @RocketMQMessageListener(topic = "springboot-mq",consumerGroup = "${rocketmq.consumer.group}")
 public class Consumer implements RocketMQListener<String> {
 
+//    @KafkaListener(id="myContainer",topics = topic,groupId = "group.demo")
+
     @Override
     public void onMessage(String s) {
         log.info("接受到消息："+ s);
     }
+
 
 }

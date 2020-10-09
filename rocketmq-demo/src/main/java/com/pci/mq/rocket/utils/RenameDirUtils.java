baf98ac4ff25ec2d502db6003dc5e588af759380
zhouyunting <zhouyunting@pcitech.com>
2020-10-09 14:02:55 +0800

    加上注释

--- rocketmq-demo/src/main/java/com/pci/mq/rocket/utils/RenameDirUtils.java
+++ rocketmq-demo/src/main/java/com/pci/mq/rocket/utils/RenameDirUtils.java
@@ -6,14 +6,15 @@ import java.util.*;
 /**
  * @author zyting
  * @sinne 2020-09-09
- * 重命名文件夹   gan0-0,gan0-25
+ * 重命名文件夹   gan1-0,gan1-25
+ * 用于中间有些文件夹被删除后，需要从1开始重新命名文件夹
  */
 public class RenameDirUtils {
 
     public static void main(String[] args) {
 
+        // 需要重命名文件夹的地址
         String path = "E:\\ganpicture";
-
         File file = new File(path);
         File[] fileArr = file.listFiles();
 
@@ -26,7 +27,7 @@ public class RenameDirUtils {
         // 判断是否可进行重命名，判断 map 中各value的list大小是否一致
         boolean flag = checkFileSize(fileMap);
         if(!flag){
-            // 如果 -0 组和 -25组的文件夹个数不一致，则跳出循环
+            // 如果 -0 组和 -25组的文件夹个数不一致，则结束代码运行
             System.out.println("分组后文件夹个数不一致！");
             return;
         }
@@ -35,7 +36,7 @@ public class RenameDirUtils {
         for(Map.Entry<String, List<File>> entry : fileMap.entrySet()){
             String key = entry.getKey();
             List<File> fileList = entry.getValue();
-            // 对文件夹进行排序
+            // 对文件夹根据名称进行排序
             fileList = sortDirByName(fileList);
             // 对个文件夹进行重命名
             renameDir(key,fileList);
@@ -44,19 +45,26 @@ public class RenameDirUtils {
     }
 
     /**
-     * 对文件夹进行重命名
-     * @param list
+     * 重命名文件夹
+     * @param key 代表是0 还是25
+     * @param list 表示-0的所有文件夹集合
      */
     private static void renameDir(String key, List<File> list) {
+        // 重命名文件夹开始位置，如果从0开始则将 a=1 改成 a=0
         int a = 1;
         for (int i = 0; i < list.size(); i++){
+            // 获取当前文件对象
             File file1 = list.get(i);
+            // 判断当前文件对象是否是文件夹
             if(file1.isDirectory()){
-                String oldPath = list.get(i).getAbsolutePath();
+                // 获取当前文件夹的绝对路径
+                String oldPath = file1.getAbsolutePath();
+                // 拼接重命名文件夹的绝对路径，如 E:\ganpicture\gan1-25
                 String newPath = file1.getParent()+"\\gan"+a+"-"+key ;
-
+                // 将文件绝对路径转换成文件对象
                 File oldFile = new File(oldPath);
                 File newFile = new File(newPath);
+                // 重命名文件夹
                 boolean b = oldFile.renameTo(newFile);
                 a++;
             }
@@ -69,17 +77,21 @@ public class RenameDirUtils {
      * @return
      */
     private static Map<String, List<File>> groupByFilter(List<File> list) {
+        // 初始化一个map，用于根据滤光片分类保存各类的文件夹
         Map<String, List<File>> map = new HashMap<>();
         for(File file :list){
             String fileName = file.getName();
+            // 获取文件夹是以0结束还是25结束
             String key = fileName.substring(fileName.lastIndexOf("-")+1);
+            // 从map中获取对应类型的文件夹集合
             List<File> files = map.get(key);
+            // 判断获取该类型的文件夹个数是否为空
             if(files != null && !files.isEmpty()){
                 // 不为空,则获取map中的list并添加这个文件对象
                 files.add(file);
                 map.put(key, files);
             }else {
-                // 为空
+                // 为空，则创建一个list集合用来保存此类型的文件夹，并将此文件夹对象添加进去
                 List<File> newFiles = new ArrayList<>();
                 newFiles.add(file);
                 map.put(key, newFiles);
@@ -89,7 +101,8 @@ public class RenameDirUtils {
     }
 
     /**
-     * 检查分组后各文件夹的大小
+     * 检查分组后各文件夹的大小；
+     * 如果-0和-25的文件夹个数不一致，则不重命名文件夹，防止顺序弄错
      * @param fileMap
      * @return
      */
--- rocketmq-demo/src/main/java/com/pci/mq/rocket/utils/RenameFileUtils.java
+++ rocketmq-demo/src/main/java/com/pci/mq/rocket/utils/RenameFileUtils.java
@@ -2,7 +2,6 @@ package com.pci.mq.rocket.utils;
 
 import java.io.File;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * @author zyting
@@ -13,8 +12,7 @@ public class RenameFileUtils {
     public static void main(String[] args) {
 
         // 执行文件夹路径，获取所有文件
-        String path = "E:\\ganpicture\\gan1-0";
-
+        String path = "F:\\test";
         File file = new File(path);
         File[] tempList = file.listFiles();
 
@@ -25,48 +23,63 @@ public class RenameFileUtils {
         list = sortFileByName(list);
 
         // 根据最后修改时间进行排序
-//        list = sortFileByLastModified(list);
+        // list = sortFileByLastModified(list);
 
-        System.out.println(list);
-
-        //重命名文件
-//        renameFile(list);
+        // 重命名文件夹中及子文件夹中的文件
+        renameFile(list);
     }
 
     /**
-     * 对排序后的文件进行重命名
+     * 先判断是否是文件夹，如果是文件夹则递归调用命名方法
      * @param list
      */
     private static void renameFile(List<File> list) {
-
+        // 命名开始数字，如果从0开始则将 a=1 改成 a=0
         int a = 1;
+        // 循环获取集合中的文件
         for (int i = 0; i < list.size(); i++){
+            // 获取当前的文件对象
             File file1 = list.get(i);
+            // 判断文件对象是否是文件夹
             if(file1.isDirectory()){
-                // 如果是
+                // 如果当前文件对象是文件夹，则获取当前文件夹中的所有文件对象
                 File[] childFiles = file1.listFiles();
+                // 将当前文件夹的文件数组转换成集合形式
                 List<File> childFileList = Arrays.asList(childFiles);
+                // 将当前文件夹的文件根据名称进行排序
+                childFileList = sortFileByName(childFileList);
+                // 递归调用重命名方法
                 renameFile(childFileList);
+                // 跳出本次循环
                 continue;
             }
-            String oldPath = list.get(i).getAbsolutePath();
-            String oldName = list.get(i).getName();
-            String newName = oldName.substring(oldName.lastIndexOf("."));
+            // 获取文件对象的绝对路径
+            String oldPath = file1.getAbsolutePath();
+            // 获取文件对象的名称
+            String oldName = file1.getName();
+            // 获取文件对象的后缀名
+            String extName = oldName.substring(oldName.lastIndexOf("."));
 
+            // 初始化重新命名文件绝对地址
             String newPath = "" ;
-            if(list.get(i).getParent().endsWith("-0")){
-                newPath = list.get(i).getParent()+"\\Y"+a+newName;
-            }else if(list.get(i).getParent().endsWith("-25")){
-                newPath = list.get(i).getParent()+"\\X"+a+newName;
+            if(file1.getParent().endsWith("-0")){
+                // 设置重新命名文件绝对地址，如果是-0 结束的文件夹则，重命名文件名前面加Y，如Y1
+                newPath = file1.getParent()+"\\Y"+a+extName;
+            }else if(file1.getParent().endsWith("-25")){
+                // 设置重新命名文件绝对地址，如果是-25 结束的文件夹则，重命名文件名前面加X，如X1
+                newPath = file1.getParent()+"\\X"+a+extName;
             }
 
+            // 将文件的绝对地址转换成文件对象
             File oldFile = new File(oldPath);
             File newFile = new File(newPath);
+            // 调用重命名方法
             boolean b = oldFile.renameTo(newFile);
             if(b){
                 System.out.println("重命名成功");
             }else{
-                System.out.println("重命名失败");
+                // 如果重命名失败则将失败路径打印出来，方便修改后重新命名
+                System.out.println("重命名失败，文件路径为："+oldPath);
             }
             a++;
         }
